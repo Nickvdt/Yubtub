d82c511aaa7e730f35e9d4a1807e5dcfb447506e
Nickvdt <32934@ma-web.nl>
2023-06-21 16:10:13 +0200

    Classe Video & Comment gemaakt en uit JSON gehaald

--- js/main.js
+++ js/main.js
@@ -24,27 +24,28 @@ class App {
     constructor() {
         this.api = new Api("./data/data.json");
 
-        this.api
-        .getData().then((data) => {
+        this.api.getData().then((data) => {
+            
             this.switcher = new Switcher(this, data);
         });
     }
 }
 
-class Switcher {
+
+
+class Switcher{
     yubtub;
     cleaner;
     app;
     default = 0;
-    constructor(app, data) {
+    constructor(app, data){
         this.data = data
         this.app = app;
-       
         this.yubtub = new Yubtub(this.app, data[this.default]);
         this.cleaner = new Cleaner();
     }
 
-    switch(link) {
+    switch(link){
         this.cleaner.clean("body");
         this.yubtub = new Yubtub(this.app, this.data[link]);
     }
@@ -68,12 +69,14 @@ class Yubtub {
         this.renderer = new Renderer(this);
         this.main = new Main(this, data);
         this.aside = new Aside(this, data);
-        this.renderer.render("body", this.main.htmlelement);
+        
     }
 }
 
+
 class Main{
     yubtub;
+    video;
     constructor(yubtub, data){
         this.yubtub = yubtub;
         this.data = data;
@@ -82,71 +85,51 @@ class Main{
         this.leftSection = document.createElement("section");
         this.leftSection.classList.add("leftSection");
 
+        this.video = new Video(data);
+        this.comments = new Comments(data);
+
+        this.mainElement.appendChild(this.leftSection);
+        this.leftSection.appendChild(this.video.sectionElement); // uit de classe video
+        this.leftSection.appendChild(this.comments.comments); // uit de classe comments
+        this.yubtub.renderer.render("body", this.mainElement);
+
+    }
+}
+
+class Video{
+    constructor(data){
+        this.data = data;
         this.sectionElement = document.createElement("section");
         this.sectionElement.classList.add("video")
 
         this.videoElement = document.createElement("video");
         this.videoElement.classList.add("video__main");
         this.videoElement.autoplay = true;
-        this.videoElement.src = "videos/video--2.mp4";
-        
-
+        this.videoElement.loop = true;
+        this.videoElement.muted = true;
+        this.videoElement.src = "/videos/" + data["video"];
         this.videoInfo = document.createElement("section");
         this.videoInfo.classList.add("video__information");
-
-        this.videoInfoLeft = document.createElement("div");
-        this.videoInfoLeft.classList.add("video__information", "video__information--left");
-
-        this.profilePictureTitle = document.createElement("img");
-        this.profilePictureTitle.src = "https://picsum.photos/80";
-        this.profilePictureTitle.classList.add("profilePicture");
-
-        this.profilePictureReaction = document.createElement("img");
-        this.profilePictureReaction.src = "https://picsum.photos/79";
-        this.profilePictureReaction.classList.add("profilePicture");
-
-        this.profilePictureReaction2 = document.createElement("img");
-        this.profilePictureReaction2.src = "https://picsum.photos/81";
-        this.profilePictureReaction2.classList.add("profilePicture");
-
         this.titleElement = document.createElement("p");
-        this.titleElement.textContent = "Titel"
+        this.titleElement.textContent = data["title"];
 
         this.videoInfoRight = document.createElement("div");
         this.videoInfoRight.classList.add("video__information", "video__information--right");
 
+        this.videoInfoLeft = document.createElement("div");
+        this.videoInfoLeft.classList.add("video__information", "video__information--left");
         this.starElement = document.createElement("i");
         this.starElement.classList.add("fa-regular", "fa-star");
 
         this.arrowElement = document.createElement("i")
         this.arrowElement.classList.add("fa-solid", "fa-arrow-right");
 
-        this.comments = document.createElement("section");
-        this.comments.classList.add("comments");
-
-        this.commentsList = document.createElement("ul");
-        this.commentsList.classList.add("comments__list");
-
-        this.commentsListItem = document.createElement("li")
-        this.commentsListItem.classList.add("comments__list", "comments__list--item");
-
-        this.commentsListItem2 = document.createElement("li")
-        this.commentsListItem2.classList.add("comments__list", "comments__list--item");
-
-        this.reactionElement = document.createElement("p");
-        this.reactionElement.textContent = "Reactie";
-
-        this.reactionElement2 = document.createElement("p");
-        this.reactionElement2.textContent = "Reactie 2";
-
-        this.textAreaElement = document.createElement("textarea");
-        this.textAreaElement.classList.add("comments__textArea");
-        this.textAreaElement.placeholder = "Plaats hier je reactie";
-
-        this.mainElement.appendChild(this.leftSection);
-        this.leftSection.appendChild(this.sectionElement);
+        this.profilePictureTitle = document.createElement("img");
+        this.profilePictureTitle.src = data["profilepicture"];
+        this.profilePictureTitle.classList.add("profilePicture");
 
         this.sectionElement.appendChild(this.videoElement);
+
         this.sectionElement.appendChild(this.videoInfo);
 
         this.videoInfo.appendChild(this.videoInfoLeft);
@@ -156,12 +139,54 @@ class Main{
         this.videoInfo.appendChild(this.videoInfoRight);
         this.videoInfoRight.appendChild(this.starElement);
         this.videoInfoRight.appendChild(this.arrowElement);
+    }
+}
 
-        this.leftSection.appendChild(this.comments);
+class Comments{
+    constructor(data){
+        this.data = data;
+     
+        this.comments = document.createElement("section");
+        this.comments.classList.add("comments");
+
+        this.commentsList = document.createElement("ul");
+        this.commentsList.classList.add("comments__list");
+
+        this.textAreaElement = document.createElement("textarea");
+        this.textAreaElement.classList.add("comments__textArea");
+        this.textAreaElement.placeholder = "Plaats hier je reactie";
+
         this.comments.appendChild(this.commentsList);
+        this.comment = new Comment(data);
 
-        this.commentsList.appendChild(this.commentsListItem);
-        this.commentsList.appendChild(this.commentsListItem2);
+        this.commentsList.appendChild(this.comment.commentsListItem);
+        this.commentsList.appendChild(this.comment.commentsListItem2);
+
+        this.comments.appendChild(this.textAreaElement);
+    }
+}
+
+class Comment{
+    constructor(data){
+        this.profilePictureReaction = document.createElement("img");
+        this.profilePictureReaction.src = data["comment1profilepicture"];
+        this.profilePictureReaction.classList.add("profilePicture");
+    
+        this.profilePictureReaction2 = document.createElement("img");
+        this.profilePictureReaction2.src = data["comment2profilepicture"];
+        this.profilePictureReaction2.classList.add("profilePicture");
+
+        this.commentsListItem = document.createElement("li")
+        this.commentsListItem.classList.add("comments__list", "comments__list--item");
+
+        this.commentsListItem2 = document.createElement("li")
+        this.commentsListItem2.classList.add("comments__list", "comments__list--item");
+
+        this.reactionElement = document.createElement("p");
+        this.reactionElement.textContent = data["comment1"];
+
+        this.reactionElement2 = document.createElement("p");
+        this.reactionElement2.textContent = data["comment2"];
 
         this.commentsListItem.appendChild(this.profilePictureReaction);
         this.commentsListItem.appendChild(this.reactionElement);
@@ -169,11 +194,8 @@ class Main{
         this.commentsListItem2.appendChild(this.profilePictureReaction2);
         this.commentsListItem2.appendChild(this.reactionElement2);
 
-        this.comments.appendChild(this.textAreaElement);
-
-        this.yubtub.renderer.render("body", this.mainElement);
-
     }
+
 }
 
 class Renderer {
